6066bd4257288832a14638935df508acc0e30191
rcmc2020 <3910895+rcmc2020@users.noreply.github.com>
2018-04-02 13:56:17 -0400

    Test cases
    Documentation and fixes for test cases.
    Added new test case.

--- test/test.js
+++ test/test.js
@@ -26,9 +26,10 @@ let ways = [
   {lts: 1, id: 'b1-9', rule: 'b1', tags: { 'highway': 'residential', 'cycleway': 'left', 'maxspeed': '30', 'lanes': '1', 'parking:lane:both': 'diagonal' }, desc: 'Bike lane, parking, cycleway=left'},
   {lts: 3, id: 'b2-1', rule: 'b2', tags: { 'highway': 'residential', 'cycleway:middle': 'lane', 'maxspeed': '30', 'lanes': '3', 'parking': 'yes' }, desc: 'Bike lane, parking, cycleway=middle'},
   {lts: 3, id: 'b2-1', rule: 'b2', tags: { 'highway': 'residential', 'cycleway:middle': 'lane', 'maxspeed': '30', 'lanes': '4', 'parking': 'yes' }, desc: 'Bike lane, parking, cycleway=middle'},
+  // FIXME: No coverage for rule B3, B4, B5 (lane width)
   {lts: 2, id: 'b6-1', rule: 'b6', tags: { 'highway': 'residential', 'cycleway': 'left', 'lanes': '2', 'parking': 'yes', 'maxspeed': '41' }, desc: 'Bike lane, parking, cycleway=left, maxspeed=41'},
   {lts: 2, id: 'b6-2', rule: 'b6', tags: { 'highway': 'residential', 'cycleway': 'left', 'lanes': '2', 'parking': 'yes', 'maxspeed': '50' }, desc: 'Bike lane, parking, cycleway=left, maxspeed=50'},
-  {lts: 2, id: 'b6-3', rule: 'b6', tags: { 'highway': 'residential', 'cycleway': 'left', 'lanes': '2', 'parking': 'yes'}, desc: 'Bike lane, parking, cycleway=left, default maxspeed'},
+  {lts: 2, id: 'b6-3', rule: 'b6', tags: { 'highway': 'residential', 'cycleway': 'left', 'lanes': '2', 'parking': 'yes' }, desc: 'Bike lane, parking, cycleway=left, default maxspeed'},
   {lts: 3, id: 'b7-1', rule: 'b7', tags: { 'highway': 'residential', 'cycleway': 'opposite', 'maxspeed': '51', 'lanes': '1', 'parking': 'yes' }, desc: 'Bike lane, parking, cycleway=opposite, maxspeed=51'},
   {lts: 3, id: 'b7-2', rule: 'b7', tags: { 'highway': 'residential', 'cycleway': 'opposite', 'maxspeed': '64', 'lanes': '1', 'parking': 'yes' }, desc: 'Bike lane, parking, cycleway=opposite, maxspeed=64'},
   {lts: 4, id: 'b8-1', rule: 'b8', tags: { 'highway': 'residential', 'cycleway': 'opposite', 'maxspeed': '65', 'lanes': '1', 'parking': 'yes' }, desc: 'Bike lane, parking, cycleway=opposite, maxspeed=65'},
@@ -41,16 +42,16 @@ let ways = [
   {lts: 1, id: 'c1-6', rule: 'c1', tags: { 'highway': 'residential', 'cycleway': 'right', 'lanes': '2' }, desc: 'Bike lane, cycleway=right'},
   {lts: 1, id: 'c1-7', rule: 'c1', tags: { 'highway': 'residential', 'cycleway': 'yes', 'lanes': '2' }, desc: 'Bike lane, cycleway=yes'},
   {lts: 1, id: 'c1-8', rule: 'c1', tags: { 'highway': 'residential', 'shoulder:access:bicycle': 'yes', 'lanes': '2' }, desc: 'Bike lane, shoulder:access:bicycle=yes'},
-  // FIXME: No coverage for rule C2
+  // FIXME: No coverage for rule C2 (separating median)
   {lts: 3, id: 'c3-1', rule: 'c3', tags: { 'highway': 'residential', 'cycleway': 'opposite', 'lanes': '3' }, desc: 'Bike lane, cycleway=opposite, lanes=3'},
   {lts: 3, id: 'c3-2', rule: 'c3', tags: { 'highway': 'residential', 'cycleway': 'opposite', 'lanes': '4' }, desc: 'Bike lane, cycleway=opposite, lanes=4'},
-  // FIXME: No coverage for rule C4
+  // FIXME: No coverage for rule C4 (lane width)
   {lts: 3, id: 'c5-1', rule: 'c5', tags: { 'highway': 'residential', 'cycleway:both': 'lane', 'maxspeed': '51' }, desc: 'Bike lane, maxspeed=51'},
   {lts: 3, id: 'c5-2', rule: 'c5', tags: { 'highway': 'residential', 'cycleway:middle': 'lane', 'maxspeed': '64' }, desc: 'Bike lane, maxspeed=64'},
   {lts: 4, id: 'c6-1', rule: 'c6', tags: { 'highway': 'residential', 'cycleway:left': 'lane', 'maxspeed': '65' }, desc: 'Bike lane, maxspeed=65'},
   {lts: 4, id: 'c6-2', rule: 'c6', tags: { 'highway': 'residential', 'cycleway:right': 'lane', 'maxspeed': '100' }, desc: 'Bike lane, maxspeed=100'},
   {lts: 4, id: 'c6-3', rule: 'c6', tags: { 'highway': 'secondary', 'cycleway:right': 'lane' }, desc: 'Bike lane, highway=seconary, maxspeed 66+ (default)'},
-  {lts: 3, id: 'c7-1', rule: 'c7', tags: { 'highway': 'tertiary', 'cycleway': 'opposite_lane'}, desc: 'Bike lane, non-residential lowspeed'},
+  {lts: 3, id: 'c7-1', rule: 'c7', tags: { 'highway': 'tertiary', 'cycleway': 'opposite_lane' }, desc: 'Bike lane, non-residential lowspeed'},
   {lts: 1, id: 'm1-1', rule: 'm1', tags: { 'highway': 'steps' }, desc: 'LTS 1: highway=steps.'},
   {lts: 2, id: 'm2-1', rule: 'm2', tags: { 'highway': 'service', 'service': 'alley' }, desc: 'LTS 2: highway=service and service=alley.'},
   {lts: 2, id: 'm3-1', rule: 'm3', tags: { 'highway': 'service', 'service': 'parking_aisle' }, desc: 'LTS 2: highway=service and service=parking_aisle.'},
@@ -64,36 +65,34 @@ let ways = [
   {lts: 3, id: 'm7-1', rule: 'm7', tags: { 'highway': 'residential', 'lanes': '4', 'maxspeed': '30' }, desc: 'Mixed traffic, highway=residential, maxspeed=30'},
   {lts: 3, id: 'm7-2', rule: 'm7', tags: { 'highway': 'residential', 'lanes': '5', 'maxspeed': '30' }, desc: 'Mixed traffic, highway=residential, maxspeed=30'},
   {lts: 3, id: 'm7-3', rule: 'm7', tags: { 'highway': 'residential', 'lanes': '5', 'maxspeed': '40' }, desc: 'Mixed traffic, highway=residential'},
-  {lts: 4, id: 'm8-1', rule: 'm8', tags: { 'highway': 'residential', 'lanes': '6', 'maxspeed': '30' }, desc: 'Mixed traffic, highway=residential'},
-  {lts: 4, id: 'm8-2', rule: 'm8', tags: { 'highway': 'residential', 'lanes': '6', 'maxspeed': '30' }, desc: 'Mixed traffic, highway=residential'},
-  {lts: 4, id: 'm8-3', rule: 'm8', tags: { 'highway': 'residential', 'lanes': '7', 'maxspeed': '40' }, desc: 'Mixed traffic, highway=residential'},
-  {lts: 2, id: 'm9-1', rule: 'm9', tags: { 'highway': 'residential', 'lanes': '2', 'maxspeed': '41' }, desc: 'Mixed traffic, highway=residential'},
-  {lts: 2, id: 'm9-2', rule: 'm9', tags: { 'highway': 'residential', 'lanes': '2', 'maxspeed': '49' }, desc: 'Mixed traffic, highway=residential'},
-  {lts: 2, id: 'm9-3', rule: 'm9', tags: { 'highway': 'residential', 'lanes': '2', 'maxspeed': '50' }, desc: 'Mixed traffic, highway=residential'},
-  {lts: 3, id: 'm10-1', rule: 'm10', tags: { 'highway': 'residential', 'lanes': '3', 'maxspeed': '41' }, desc: 'Mixed traffic, highway=residential'},
-  {lts: 3, id: 'm10-2', rule: 'm10', tags: { 'highway': 'residential', 'lanes': '3', 'maxspeed': '49' }, desc: 'Mixed traffic, highway=residential'},
-  {lts: 3, id: 'm10-3', rule: 'm10', tags: { 'highway': 'residential', 'lanes': '3', 'maxspeed': '50' }, desc: 'Mixed traffic, highway=residential'},
-  {lts: 3, id: 'm10-4', rule: 'm10', tags: { 'highway': 'tertiary', 'lanes': '2', 'maxspeed': '41' }, desc: 'Mixed traffic, highway=tertiary'},
-  {lts: 3, id: 'm10-5', rule: 'm10', tags: { 'highway': 'tertiary', 'lanes': '3', 'maxspeed': '49' }, desc: 'Mixed traffic, highway=tertiary'},
-  {lts: 3, id: 'm10-6', rule: 'm10', tags: { 'highway': 'tertiary', 'lanes': '3', 'maxspeed': '50' }, desc: 'Mixed traffic, highway=tertiary'},
-  {lts: 3, id: 'm10-7', rule: 'm10', tags: { 'highway': 'secondary', 'lanes': '2', 'maxspeed': '41' }, desc: 'Mixed traffic, highway=residential'},
-  {lts: 4, id: 'm11-1', rule: 'm11', tags: { 'highway': 'residential', 'lanes': '4', 'maxspeed': '41' }, desc: 'Mixed traffic, highway=residential'},
-  {lts: 4, id: 'm11-2', rule: 'm11', tags: { 'highway': 'residential', 'lanes': '4', 'maxspeed': '49' }, desc: 'Mixed traffic, highway=residential'},
-  {lts: 4, id: 'm11-3', rule: 'm11', tags: { 'highway': 'residential', 'lanes': '4', 'maxspeed': '50' }, desc: 'Mixed traffic, highway=residential'},
-  {lts: 4, id: 'm11-4', rule: 'm11', tags: { 'highway': 'tertiary', 'lanes': '5', 'maxspeed': '41' }, desc: 'Mixed traffic, highway=tertiary'},
-  {lts: 4, id: 'm11-5', rule: 'm11', tags: { 'highway': 'tertiary', 'lanes': '5', 'maxspeed': '49' }, desc: 'Mixed traffic, highway=tertiary'},
-  {lts: 4, id: 'm11-6', rule: 'm11', tags: { 'highway': 'tertiary', 'lanes': '5', 'maxspeed': '50' }, desc: 'Mixed traffic, highway=tertiary'},
+  {lts: 4, id: 'm8-1', rule: 'm8', tags: { 'highway': 'residential', 'lanes': '6', 'maxspeed': '30' }, desc: 'Mixed traffic, highway=residential, maxspeed=30'},
+  {lts: 4, id: 'm8-2', rule: 'm8', tags: { 'highway': 'residential', 'lanes': '6', 'maxspeed': '30' }, desc: 'Mixed traffic, highway=residential, maxspeed=30'},
+  {lts: 4, id: 'm8-3', rule: 'm8', tags: { 'highway': 'residential', 'lanes': '7', 'maxspeed': '40' }, desc: 'Mixed traffic, highway=residential, maxspeed=40'},
+  {lts: 2, id: 'm9-1', rule: 'm9', tags: { 'highway': 'residential', 'lanes': '2', 'maxspeed': '41' }, desc: 'Mixed traffic, highway=residential, maxspeed=41'},
+  {lts: 2, id: 'm9-2', rule: 'm9', tags: { 'highway': 'residential', 'lanes': '2', 'maxspeed': '49' }, desc: 'Mixed traffic, highway=residential, maxspeed=49'},
+  {lts: 2, id: 'm9-3', rule: 'm9', tags: { 'highway': 'residential', 'lanes': '2', 'maxspeed': '50' }, desc: 'Mixed traffic, highway=residential, maxspeed=50'},
+  {lts: 3, id: 'm10-1', rule: 'm10', tags: { 'highway': 'residential', 'lanes': '3', 'maxspeed': '41' }, desc: 'Mixed traffic, highway=residential, maxspeed=41'},
+  {lts: 3, id: 'm10-2', rule: 'm10', tags: { 'highway': 'residential', 'lanes': '3', 'maxspeed': '49' }, desc: 'Mixed traffic, highway=residential, maxspeed=49'},
+  {lts: 3, id: 'm10-3', rule: 'm10', tags: { 'highway': 'residential', 'lanes': '3', 'maxspeed': '50' }, desc: 'Mixed traffic, highway=residential, maxspeed=50'},
+  {lts: 3, id: 'm10-4', rule: 'm10', tags: { 'highway': 'tertiary', 'lanes': '2', 'maxspeed': '41' }, desc: 'Mixed traffic, highway=tertiary, maxspeed=41'},
+  {lts: 3, id: 'm10-5', rule: 'm10', tags: { 'highway': 'tertiary', 'lanes': '3', 'maxspeed': '49' }, desc: 'Mixed traffic, highway=tertiary, maxspeed=49'},
+  {lts: 3, id: 'm10-6', rule: 'm10', tags: { 'highway': 'tertiary', 'lanes': '3', 'maxspeed': '50' }, desc: 'Mixed traffic, highway=tertiary, maxspeed=50'},
+  {lts: 3, id: 'm10-7', rule: 'm10', tags: { 'highway': 'secondary', 'lanes': '2', 'maxspeed': '41' }, desc: 'Mixed traffic, highway=residential, maxspeed=41'},
+  {lts: 4, id: 'm11-1', rule: 'm11', tags: { 'highway': 'residential', 'lanes': '4', 'maxspeed': '41' }, desc: 'Mixed traffic, highway=residential, maxspeed=41'},
+  {lts: 4, id: 'm11-2', rule: 'm11', tags: { 'highway': 'residential', 'lanes': '4', 'maxspeed': '49' }, desc: 'Mixed traffic, highway=residential, maxspeed=49'},
+  {lts: 4, id: 'm11-3', rule: 'm11', tags: { 'highway': 'residential', 'lanes': '4', 'maxspeed': '50' }, desc: 'Mixed traffic, highway=residential, maxspeed=50'},
+  {lts: 4, id: 'm11-4', rule: 'm11', tags: { 'highway': 'tertiary', 'lanes': '5', 'maxspeed': '41' }, desc: 'Mixed traffic, highway=tertiary, maxspeed=41'},
+  {lts: 4, id: 'm11-5', rule: 'm11', tags: { 'highway': 'tertiary', 'lanes': '5', 'maxspeed': '49' }, desc: 'Mixed traffic, highway=tertiary, maxspeed=49'},
+  {lts: 4, id: 'm11-6', rule: 'm11', tags: { 'highway': 'tertiary', 'lanes': '5', 'maxspeed': '50' }, desc: 'Mixed traffic, highway=tertiary, maxspeed=50'},
   {lts: 4, id: 'm12-1', rule: 'm12', tags: { 'highway': 'residential', 'lanes': '1', 'maxspeed': '51' }, desc: 'Mixed traffic, highway=residential, lanes=4'},
-  {lts: 4, id: 'm12-1', rule: 'm12', tags: { 'highway': 'primary', 'lanes': '1', 'maxspeed': '51' }, desc: 'Mixed traffic, maxspeed 51+'}
+  {lts: 4, id: 'm12-2', rule: 'm12', tags: { 'highway': 'primary', 'lanes': '1', 'maxspeed': '51' }, desc: 'Mixed traffic, maxspeed 51+'},
+  {lts: 4, id: 'm12-3', rule: 'm12', tags: { 'highway': 'primary', 'lanes': '1'}, desc: 'Mixed traffic, default maxspeed on primary highway'}
 ]
 
 test('Stressmodel General Tests', (assert) => {
   assert.equal(stressmodel.name, 'default', 'g1 - Name matches \'default\'')
-
   assert.equal(stressmodel.levels, 4, 'g2 - Number of levels=4')
-
   assert.equal(typeof stressmodel.description, 'string', 'g3 - Description must be a string.')
-
   assert.end()
 })
 
